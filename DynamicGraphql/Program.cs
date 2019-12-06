7bad0de240872a84cc492433fb49eec64f801624
Paolo Galfione <paolo.galfione@softwareuno.it>
2019-12-06 14:54:39 +0100

    poc with error

--- DynamicGraphql/Program.cs
+++ DynamicGraphql/Program.cs
@@ -1,37 +1,32 @@
-﻿using System;
-using System.Threading.Tasks;
-using HelloWorldCode;
-using HotChocolate;
+﻿using HotChocolate;
 using HotChocolate.Execution;
-using HotChocolate.Language;
+using HotChocolate.Resolvers;
 using HotChocolate.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace HelloWord
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            ExecuteGraphQL().Wait();
-        }
+            // In real application I store template on a DB table configurable by user
+            // eg. {model="Survey", fields={ {name="id", type="IdType"}, {name="stars", type="IntType"}, {name="commentary", type="StringType"} } }
 
-        static async Task ExecuteGraphQL()
-        {
+            // create ObjectType from template 
             ObjectType survey = new ObjectType(c =>
             {
                 c.Name("Survey");
-                c.Field<JsonQL>(r => r.GetElement(default)).Type<IdType>().Name("id");
-                c.Field<JsonQL>(r => r.GetElement(default)).Type<IntType>().Name("stars");
-                c.Field<JsonQL>(r => r.GetElement(default)).Type<StringType>().Name("commentary");
+                c.Field<JsonResolvers>(r => r.GetProperty(default)).Type<IdType>().Name("id");
+                c.Field<JsonResolvers>(r => r.GetProperty(default)).Type<IntType>().Name("stars");
+                c.Field<JsonResolvers>(r => r.GetProperty(default)).Type<StringType>().Name("commentary");
             });
 
-            ObjectType query = new ObjectType(c =>
-            {
-                c.Name("Query");
-                c.Field<JsonQL>(r => r.GetById(default)).Type(survey).Argument("id", a => a.Type<NonNullType<IdType>>()).Name("survey");
-                c.Field<JsonQL>(r => r.GetAll()).Type(new ListType(survey)).Name("surveys");
-            });
-
+            // create InputObjectType from template 
             InputObjectType surveyInput = new InputObjectType(c =>
             {
                 c.Name("SurveyInput");
@@ -39,6 +34,16 @@ namespace HelloWord
                 c.Field("stars").Type<NonNullType<IntType>>();
                 c.Field("commentary").Type<NonNullType<StringType>>();
             });
+
+
+            ObjectType query = new ObjectType(c =>
+            {
+                c.Name("Query");
+                c.Field<JsonResolvers>(r => r.GetDocumentById(default)).Type(survey).Argument("id", a => a.Type<NonNullType<IdType>>()).Name("survey");
+                c.Field<JsonResolvers>(r => r.GetDocuments()).Type(new ListType(survey)).Name("surveys");
+
+            });
+
 
             ObjectType mutation = new ObjectType(c =>
             {
@@ -77,6 +82,60 @@ namespace HelloWord
 
             var result = await executor.ExecuteAsync(request);
             Console.WriteLine(result.ToJson());
+        }
+    }
+
+    class JsonResolvers
+    {
+        private class Item
+        {
+            public int Id { get; set; }
+            public string Json { get; set; }
+        }
+        private List<Item> data;
+        public JsonResolvers()
+        {
+            data = new List<Item>();
+            data.Add(new Item { Id = 1, Json = "{\"id\":1,\"stars\":5,\"commentary\":\"beautiful\"}" });
+            data.Add(new Item { Id = 2, Json = "{\"id\":2,\"stars\":4,\"commentary\":\"it works!\"}" });
+        }
+
+        public IEnumerable<JsonDocument> GetDocuments()
+        {
+            var results = data.Select(e => JsonDocument.Parse(e.Json));
+            return results;
+        }
+        public JsonDocument GetDocumentById(int id)
+        {
+            var item = data.FirstOrDefault(d => d.Id == id);
+            var document = JsonDocument.Parse(item.Json);
+            return document;
+        }
+
+        public object GetProperty([Parent] IResolverContext parent)
+        {
+            var name = parent.Field.Name.Value;
+            var type = parent.Field.Type;
+            var document = parent.Source.First() as JsonDocument;
+            if (type is IdType)
+            {
+                return document.RootElement.GetProperty(name).GetInt32();
+            }
+            if (type is IntType)
+            {
+                return document.RootElement.GetProperty(name).GetInt32();
+            }
+            if (type is StringType)
+            {
+                return document.RootElement.GetProperty(name).GetString();
+            }
+            return null;
+        }
+
+        public JsonDocument Upsert(object input)
+        {
+            // store object in json format in data storage
+            return null;
         }
     }
 }
