30fd102468c175d87c63adf118f468303da30172
Paolo Galfione <paolo.galfione@softwareuno.it>
2019-12-06 12:52:58 +0100

    test program

---
+++ DynamicGraphql/JsonQL.cs
@@ -0,0 +1,99 @@
+﻿using HotChocolate;
+using HotChocolate.Resolvers;
+using HotChocolate.Types;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace HelloWorldCode
+{
+    public class JsonQL
+    {
+        private SingletonDataContainer data = SingletonDataContainer.Instance;
+
+        public IEnumerable<JsonDocument> GetAll()
+        {
+            IEnumerable<JsonDocument> results = data.GetAll().Select(e => JsonDocument.Parse(e.Json));
+            return results;
+        }
+        public JsonDocument GetById(int id)
+        {
+            JsonItem item = data.GetById(id);
+            var document = JsonDocument.Parse(item.Json);
+            return document;
+        }
+
+        public object GetElement([Parent] IResolverContext parent)
+        {
+            var name = parent.Field.Name.Value;
+            IOutputType tipo = parent.Field.Type;
+            JsonDocument document = parent.Source.First() as JsonDocument;
+            object result = null;
+            if (tipo is IdType)
+            {
+                var intRes = document.RootElement.GetProperty(name).GetInt32();
+                result = intRes;
+            }
+            if (tipo is IntType)
+            {
+                var intRes = document.RootElement.GetProperty(name).GetInt32();
+                result = intRes;
+            }
+            if (tipo is StringType)
+            {
+                var stringRes = document.RootElement.GetProperty(name).GetString();
+                result = stringRes;
+            }
+            return result;
+        }
+
+        public JsonDocument Upsert(object input)
+        {
+            // store object in json format in data storage
+            return null;
+        }
+    }
+
+    public class JsonItem
+    {
+        public int Id { get; set; }
+        public string Json { get; set; }
+    }
+
+    public class SingletonDataContainer
+    {
+        private List<JsonItem> items = new List<JsonItem>();
+
+        private SingletonDataContainer()
+        {
+            items.Add(new JsonItem { Id = 1, Json = "{\"id\":1,\"stars\":5,\"commentary\":\"beautiful\"}" });
+            items.Add(new JsonItem { Id = 2, Json = "{\"id\":2,\"stars\":4,\"commentary\":\"it works!\"}" });
+        }
+        public IEnumerable<JsonItem> GetAll()
+        {
+            return items;
+        }
+
+        public JsonItem GetById(int id)
+        {
+            return items.FirstOrDefault(i => i.Id == id);
+        }
+
+        public void Upsert(JsonItem item)
+        {
+            if (item.Id == 0)
+            {
+                items.Add(item);
+            }
+            else
+            {
+                var index = items.FindIndex(i => i.Id == item.Id);
+                items[index].Json = item.Json;
+            }
+        }
+
+        private static readonly SingletonDataContainer instance = new SingletonDataContainer();
+
+        public static SingletonDataContainer Instance => instance;
+    }
+}
---
+++ DynamicGraphql/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading.Tasks;
+using HelloWorldCode;
+using HotChocolate;
+using HotChocolate.Execution;
+using HotChocolate.Language;
+using HotChocolate.Types;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteGraphQL().Wait();
+        }
+
+        static async Task ExecuteGraphQL()
+        {
+            ObjectType survey = new ObjectType(c =>
+            {
+                c.Name("Survey");
+                c.Field<JsonQL>(r => r.GetElement(default)).Type<IdType>().Name("id");
+                c.Field<JsonQL>(r => r.GetElement(default)).Type<IntType>().Name("stars");
+                c.Field<JsonQL>(r => r.GetElement(default)).Type<StringType>().Name("commentary");
+            });
+
+            ObjectType query = new ObjectType(c =>
+            {
+                c.Name("Query");
+                c.Field<JsonQL>(r => r.GetById(default)).Type(survey).Argument("id", a => a.Type<NonNullType<IdType>>()).Name("survey");
+                c.Field<JsonQL>(r => r.GetAll()).Type(new ListType(survey)).Name("surveys");
+            });
+
+            InputObjectType surveyInput = new InputObjectType(c =>
+            {
+                c.Name("SurveyInput");
+                c.Field("id").Type<NonNullType<IdType>>();
+                c.Field("stars").Type<NonNullType<IntType>>();
+                c.Field("commentary").Type<NonNullType<StringType>>();
+            });
+
+            ObjectType mutation = new ObjectType(c =>
+            {
+                c.Name("Mutation");
+/*  ERROR          c.Field<JsonQL>(r => r.Upsert(default))
+                    .Type(survey)
+                    .Argument("survey", d => d.Type(surveyInput))
+                    .Name("upsertSurvey"); */
+            });
+
+            ISchema schema = SchemaBuilder.New()
+                    .AddQueryType(query)
+                    .AddMutationType(mutation)
+                    .AddType(survey)
+                    .AddType(surveyInput)
+                    .Create();
+
+            IQueryExecutor executor = schema.MakeExecutable();
+
+            Console.WriteLine(schema.ToString());
+            Console.WriteLine("-------------------------------------------------------------");
+
+            /* 
+            var request = QueryRequestBuilder.New()
+                .SetQuery("mutation { upsertSurvey(servey: { id: 100, stars: 3, commentary: \"good\" }) { stars commentary } }")
+                .Create();
+
+            IExecutionResult result = await executor.ExecuteAsync(request);
+            Console.WriteLine(result.ToJson());
+            Console.WriteLine("-------------------------------------------------------------");
+            */
+
+            var request = QueryRequestBuilder.New()
+                            .SetQuery("{ survey(id: 2) { id stars commentary } surveys { id stars commentary } }")
+                            .Create();
+
+            var result = await executor.ExecuteAsync(request);
+            Console.WriteLine(result.ToJson());
+        }
+    }
+}
+
